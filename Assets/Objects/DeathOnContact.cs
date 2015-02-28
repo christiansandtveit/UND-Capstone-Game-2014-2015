0729e3b32bd22d6ed2590dbce16d138904c73345
christiansandtveit <christian.sandtveit@my.und.edu>
2015-02-27 20:24:39 -0600

    Updated Scripts to stop puzzle when player dies
    Updated DeathOnContact.cs and InteractTimeTrialLever scripts, so that
    when player dies, the puzzle is deactivated, instead of continuing to be
    active when the player respawn

--- Assets/Objects/DeathOnContact.cs
+++ Assets/Objects/DeathOnContact.cs
@@ -5,11 +5,25 @@ using System.Collections;
 
 [RequireComponent(typeof(Collider))]
 public class DeathOnContact : MonoBehaviour {
+
+    public static bool playerDead; //Static variable, will be accessed in other scripts
+
+    // Use this for initialization
+    void Start()
+    {
+        playerDead = false;
+    }
+
+
+
 	void OnCollisionEnter(Collision other)
 	{
 		if (other.gameObject.tag == "Player")
 		{
 			other.transform.root.GetComponent<Manager>().Kill ();
+            playerDead = true;
+            Invoke("DeactivatePlayerDead", 2.0F);
+
 		}
 	}
 
@@ -18,6 +32,14 @@ public class DeathOnContact : MonoBehaviour {
 		if (other.gameObject.tag == "Player")
 		{
 			other.transform.root.GetComponent<Manager>().Kill ();
+            playerDead = true;
+            Invoke("DeactivatePlayerDead", 2.0F);
 		}
 	}
+
+    //Functiont to deactivate playerDead
+    void DeactivatePlayerDead()
+    {
+        playerDead = false;
+    }
 }
