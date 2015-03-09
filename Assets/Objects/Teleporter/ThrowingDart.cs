6ac7384106f8646ddc99eb4f6fdb7f33d62e3b01
dwinger <darrin.d.winger@my.und.edu>
2015-03-09 15:45:01 -0500

    add objects and scripts teleporter level

---
+++ Assets/Objects/Teleporter/ThrowingDart.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowingDart : Carriable {
+
+	// Use this for initialization
+    void OnCollisionEnter(Collision col)
+    {
+        if (!audio.isPlaying)
+        {
+            audio.Play();
+        }
+    }
+}
