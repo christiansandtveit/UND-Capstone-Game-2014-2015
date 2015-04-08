c2999eaa775ce50a6960a00a0ae8b490813d0357
christiansandtveit <christian.sandtveit@my.und.edu>
2015-04-08 16:14:28 -0500

    Started to finalize time trial level
    - Added tree, bush and building objects
    - Made a script which turns the walls invisible in timetrial level

--- Assets/Player/Scripts/MouseLook.cs
+++ Assets/Player/Scripts/MouseLook.cs
@@ -1,55 +1,53 @@
-//Michael Schilling
-//CSci 448
 using UnityEngine;
 using System.Collections;
 
 /// MouseLook rotates the transform based on the mouse delta.
 /// Minimum and Maximum values can be used to constrain the possible rotation
 
+/// To make an FPS style character:
+/// - Create a capsule.
+/// - Add the MouseLook script to the capsule.
+///   -> Set the mouse look to use LookX. (You want to only turn character but not tilt it)
+/// - Add FPSInputController script to the capsule
+///   -> A CharacterMotor and a CharacterController component will be automatically added.
+
+/// - Create a camera. Make the camera a child of the capsule. Reset it's transform.
+/// - Add a MouseLook script to the camera.
+///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
 [AddComponentMenu("Camera-Control/Mouse Look")]
 public class MouseLook : MonoBehaviour {
 
 	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
 	public RotationAxes axes = RotationAxes.MouseXAndY;
-	public float sensitivityX = 10.0f;
-	public float sensitivityY = 10.0f;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
 
-    // How much of the original sensitivity will be used while sprinting?
-    // For example, if sensitivity is 15 and sprintSensitivityModifier is 0.1f,
-    // then sprinting sensitivity will be 1.5f
+	public float minimumX = -360F;
+	public float maximumX = 360F;
 
-    private float currentSensitivityX;
-    private float currentSensitivityY;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
 
-	public float minimumX = -360.0f;
-	public float maximumX = 360.0f;
-
-	public float minimumY = -60.0f;
-	public float maximumY = 60.0f;
-
-	float rotationY = 0.0f;
-
-    public CharacterMotor characterMotor;
+	float rotationY = 0F;
 
 	void Update ()
 	{
-        UpdateCurrentSensitivity ();
 		if (axes == RotationAxes.MouseXAndY)
 		{
-            float rotationX = transform.localEulerAngles.y + Input.GetAxis ("Mouse X") * currentSensitivityX;
-
-            rotationY += Input.GetAxis ("Mouse Y") * currentSensitivityY;
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
 			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		}
 		else if (axes == RotationAxes.MouseX)
 		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * currentSensitivityX, 0);
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
 		}
 		else
 		{
-            rotationY += Input.GetAxis ("Mouse Y") * currentSensitivityY;
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
 			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
 			
 			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
@@ -58,20 +56,8 @@ public class MouseLook : MonoBehaviour {
 	
 	void Start ()
 	{
-        //characterMotor = transform.parent.GetComponent<CharacterMotor> ();
+		// Make the rigid body not change rotation
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
 	}
-
-    void UpdateCurrentSensitivity ()
-    {
-        if (Input.GetButton("Sprint") && characterMotor.LocalizedInput.z > 0.0f)
-        {
-            currentSensitivityX = sensitivityX * 0.6f;
-            currentSensitivityY = sensitivityY * 0.6f;
-        }
-        else
-        {
-            currentSensitivityX = sensitivityX;
-            currentSensitivityY = sensitivityY;
-        }
-    }
 }
