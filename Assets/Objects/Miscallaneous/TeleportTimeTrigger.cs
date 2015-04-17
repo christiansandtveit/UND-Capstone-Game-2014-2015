26d0e08c5059690ade91b9e260156541a48f29b1
christiansandtveit <christian.sandtveit@my.und.edu>
2015-04-17 14:11:00 -0500

    Worked on level3Hub
    - Changed the outline a little. Made one of the paths shorter, since it
    took very long time to run through it
    - Added taller mountains in the background, and painted the highest
    mountain tops with an ice texture, to give the impression of snow caps
    - Added trees near the tunnel entrance which will take the player to the
    timetrial level, which has a forest theme
    - Modified EndingScript. Will now allow the player to teleport to
    timetrial level by pressing E near the tunnel entrance.
    - Added a script which will tell the user to press e to enter the
    tunnel, which will teleport the player to the timetrial level

---
+++ Assets/Objects/Miscallaneous/TeleportTimeTrigger.cs
@@ -0,0 +1,39 @@
+﻿//Christian Oliver Sandtveit
+//Script which will create a trigger which will show a hint to the player when entered
+
+using UnityEngine;
+using System.Collections;
+
+public class TeleportTimeTrigger : MonoBehaviour
+{
+    //Variables
+    bool showHint;
+
+    // Use this for initialization
+    void Start()
+    {
+        showHint = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            showHint = true;
+            Invoke("HideHint", 5.0F);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (showHint == true)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200), "Press E to enter!");
+        }
+    }
+
+    void HideHint()
+    {
+        showHint = false;
+    }
+}
