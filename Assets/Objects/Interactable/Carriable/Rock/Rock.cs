9d7196c1ddb3065f5964b7832f7a0981f3d1a3eb
MatthewNelson2015 <matthew.b.nelson@my.und.edu>
2016-02-01 10:06:14 -0600

    Bad rock fix
    Fixed the rocks so they will not spin around in the hand of the user
    anymore by resetting the velocity to 0 constantly when it is in the
    users hand will experiment with making it kinetic instead.

--- Assets/Objects/Interactable/Carriable/Rock/Rock.cs
+++ Assets/Objects/Interactable/Carriable/Rock/Rock.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 using System.Collections;
 
 public class Rock : Carriable {
+    public Rigidbody rockBoddy;
+    void Start()
+    {
+        rockBoddy = GetComponent<Rigidbody>();
+    }
 	void OnCollisionEnter(Collision col) {
 		if (!GetComponent<AudioSource>().isPlaying) {
 			GetComponent<AudioSource>().Play ();
 		}
+    }
+    void FixedUpdate()
+    {
+        if(isBeingCarried)
+        {
+            rockBoddy.velocity = new Vector3(0, 0);
+        }
     }
 
 }
