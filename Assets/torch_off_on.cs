4c046829ea0f7598c3b1536dcc9a3874e1038373
MatthewNelson2015 <matthew.b.nelson@my.und.edu>
2016-02-06 19:42:05 -0600

    trigger fields part one
    added a trigger field to level one to turn some lights off so that unity
    will light up the area your in and not the ones your not in

---
+++ Assets/torch_off_on.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class torch_off_on : Activatable{
+
+    private Light[] lights;
+    private ParticleSystem flames;
+    private bool isActive = true;
+
+    public bool Active
+    {
+        get { return isActive; }
+    }
+
+    void Awake()
+    {
+        lights = gameObject.GetComponentsInChildren<Light>();
+        flames = gameObject.GetComponentInChildren<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (isActive == true)
+        {
+            if (GetComponent<AudioSource>().isPlaying == false)
+            {
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        else
+        {
+            GetComponent<AudioSource>().Stop();
+        }
+    }
+
+    public override void Deactivate()
+    {
+        isActive = false;
+        for (int i = 0; i < lights.Length; i++)
+        {
+            lights[i].enabled = false;
+        }
+        flames.Pause();
+        flames.Clear();
+    }
+
+    public override void Activate()
+    {
+        isActive = true;
+        for (int i = 0; i < lights.Length; i++)
+        {
+            lights[i].enabled = true;
+        }
+        flames.Play();
+    }
+
+    public void Toggle()
+    {
+        if (isActive)
+        {
+            isActive = false;
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].enabled = false;
+            }
+            flames.Pause();
+            flames.Clear();
+        }
+        else
+        {
+            isActive = true;
+            for (int i = 0; i < lights.Length; i++)
+            {
+                lights[i].enabled = true;
+            }
+            flames.Play();
+        }
+    }
+
+    public override void Reset() { }
+}
