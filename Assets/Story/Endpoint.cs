c3ffe80993d7bcc41b8a59623c9c5a7cc9363eca
MatthewNelson2015 <matthew.b.nelson@my.und.edu>
2016-04-08 10:10:10 -0500

    falling level ending added
    Added the ending to new level and added it to the build list and also
    added it to the options of levels from the level select menu

--- Assets/Story/Endpoint.cs
+++ Assets/Story/Endpoint.cs
@@ -1,6 +1,7 @@
 ﻿//Randall Howatt
 //CSci 448
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class Endpoint : MonoBehaviour {
@@ -13,11 +14,11 @@ public class Endpoint : MonoBehaviour {
 		{
 			if (currentLevel == 1) {
 				menuMain.time1 = FindObjectOfType<guiTime> ().Get ();
-				Application.LoadLevel ("ScoreBoard1");
+                SceneManager.LoadScene("ScoreBoard1");
 			}
 			else if (currentLevel == 2) {
 				menuMain.time2 = FindObjectOfType<guiTime> ().Get ();
-				Application.LoadLevel ("level3Hub");
+                SceneManager.LoadScene("level3Hub");
 			}
             else if (currentLevel == 13)
             {
@@ -25,7 +26,12 @@ public class Endpoint : MonoBehaviour {
                 KeyManager.teleKeyCollected = false;
                 KeyManager.iceKeyCollected = false;
                 menuMain.time2 = FindObjectOfType<guiTime>().Get();
-                Application.LoadLevel("Ending");
+                SceneManager.LoadScene("FallingLevel");
+            }
+            else if(currentLevel == 14)
+            {
+                menuMain.time2 = FindObjectOfType<guiTime>().Get();
+                SceneManager.LoadScene("Ending");
             }
 		}
 	}
