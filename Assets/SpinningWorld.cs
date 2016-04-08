53a41255daac0753a5d562d101208088933a1790
MatthewNelson2015 <matthew.b.nelson@my.und.edu>
2016-04-08 11:46:54 -0500

    falling level update 2
    Updated the falling level by adding the first puzzle area and it is
    under the game object fist test

---
+++ Assets/SpinningWorld.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinningWorld : MonoBehaviour {
+
+    public int randomLowObject = 0;
+    public int randomHighObject = 5;
+    public int randomLowWorld = 0;
+    public int randomhighWorld = 3;
+    private int turnSpeedObject = 0;
+    private int turnSpeedWorld = 0;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        turnSpeedObject = Random.Range(randomLowObject, randomHighObject);
+        turnSpeedWorld = Random.Range(randomLowWorld, randomLowWorld);
+        transform.Rotate(Vector3.right, turnSpeedObject * Time.deltaTime);
+        transform.Rotate(Vector3.up, turnSpeedObject * Time.deltaTime);
+    }
+}
